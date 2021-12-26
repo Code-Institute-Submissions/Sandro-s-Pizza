074616dae3980859da3a74e9bd310bb491b42e4c
bencheee <sandro.bencinic@gmail.com>
2021-12-26 11:46:53 +0000

    Add function to swipe carousel images on mobile

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,21 @@
+// Adds event listener to nav toggle button on mobile
 $(".navbar__button").click(function(){
     $(".navbar__menu").toggle( "slide" );
 })
 
-
-function swipeleftHandler() {
-    alert("swiped")
+// Adds swipe event to pizza image on index page on mobile phones
+// CODE CREDIT: https://stackoverflow.com/a/56663695
+let touchstartX = 0
+let touchendX = 0
+const slider = $(".pizza-carousel__item")[0]
+function handleGesture() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
 }
-$(".pizza-carousel__item").on( "swipeleft", swipeleftHandler );
+slider.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+slider.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  handleGesture()
+})
