6671bea1d1fa6d7a8a3dba82051b7b60431a94f8
bencheee <sandro.bencinic@gmail.com>
2021-12-27 14:19:55 +0000

    Add item page

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import menu
+from .views import menu, item
 
 urlpatterns = [
     path('menu/', menu, name='menu'),
+    path('item/', item, name='item'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def menu(request):
     return render(request, 'product/menu.html')
+
+
+def item(request):
+    return render(request, 'product/item.html')
