29c71e6f83882a35f798239ccc82ac166b39d0ac
bencheee <sandro.bencinic@gmail.com>
2021-12-28 12:02:36 +0000

    Add contact us page

---
+++ home/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+	first_name = forms.CharField(max_length = 50, widget=forms.TextInput(attrs={'placeholder': 'First name'}))
+	last_name = forms.CharField(max_length = 50, widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
+	email_address = forms.EmailField(max_length = 150, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+	message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your message'}))
--- home/views.py
+++ home/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def contact(request):
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			subject = "Website Inquiry" 
+			body = {
+			'first_name': form.cleaned_data['first_name'], 
+			'last_name': form.cleaned_data['last_name'], 
+			'email': form.cleaned_data['email_address'], 
+			'message':form.cleaned_data['message'], 
+			}
+			message = "\n".join(body.values())
+
+			try:
+				send_mail(subject, message, 'sandro.bencinic@gmail.com', ['sandro.bencinic@gmail.com']) 
+			except BadHeaderError:
+				return HttpResponse('Invalid header found.')
+			return redirect ("index")
+      
+	form = ContactForm()
+	return render(request, "home/contact.html", {'form':form})
