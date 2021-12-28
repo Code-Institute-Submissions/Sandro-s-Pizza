5b39b1a81e325c2a526fb0ded2531ecb722e0cd8
bencheee <sandro.bencinic@gmail.com>
2021-12-28 12:32:58 +0000

    Add message sent page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, contact
+from .views import index, contact, message_sent
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
+    path('message_sent/', message_sent, name='message_sent'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,28 +3,33 @@ from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'home/index.html')
 
 
 def contact(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			subject = "Website Inquiry" 
-			body = {
-			'first_name': form.cleaned_data['first_name'], 
-			'last_name': form.cleaned_data['last_name'], 
-			'email': form.cleaned_data['email_address'], 
-			'message':form.cleaned_data['message'], 
-			}
-			message = "\n".join(body.values())
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            body = {
+                'first_name': form.cleaned_data['first_name'],
+                'last_name': form.cleaned_data['last_name'],
+                'email': form.cleaned_data['email_address'],
+                'message': form.cleaned_data['message'],
+            }
+            message = "\n".join(body.values())
+            try:
+                send_mail(
+                    subject, message, body['email'],
+                    ['sandro.bencinic@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect("message_sent")
+        form = ContactForm()
+    return render(request, "home/contact.html", {'form': form})
 
-			try:
-				send_mail(subject, message, 'sandro.bencinic@gmail.com', ['sandro.bencinic@gmail.com']) 
-			except BadHeaderError:
-				return HttpResponse('Invalid header found.')
-			return redirect ("index")
-      
-	form = ContactForm()
-	return render(request, "home/contact.html", {'form':form})
+
+def message_sent(request):
+    return render(request, "home/message_sent.html")
