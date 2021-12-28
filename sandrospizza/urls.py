e1c2f7bfa621a5ca07652bba84f3d0b522a47885
bencheee <sandro.bencinic@gmail.com>
2021-12-28 09:48:40 +0000

    Add order app

--- sandrospizza/urls.py
+++ sandrospizza/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('product/', include('product.urls')),
+    path('order/', include('order.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
