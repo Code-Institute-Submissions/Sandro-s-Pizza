59c404b291d219c681073bfc28d4c60aafeb723e
bencheee <sandro.bencinic@gmail.com>
2021-12-29 09:03:13 +0000

    Add checkout complete page

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import checkout
+from .views import checkout, checkout_complete
 
 urlpatterns = [
     path('checkout/', checkout, name='checkout'),
+    path('checkout_complete/', checkout_complete, name='checkout_complete'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def checkout(request):
     return render(request, 'checkout/checkout.html')
+
+
+def checkout_complete(request):
+    return render(request, 'checkout/checkout_complete.html')
+
