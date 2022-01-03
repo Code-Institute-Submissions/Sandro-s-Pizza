0123e7fe1e57b3a565c44e31966884b9a3461d10
bencheee <sandro.bencinic@gmail.com>
2022-01-03 12:24:17 +0000

    Add add_item function to order app

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import order
+from .views import order, add_item
 
 urlpatterns = [
     path('', order, name='order'),
+    path('add_item/<item_id>', add_item, name='add_item'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def order(request):
     return render(request, 'order/order.html')
+
+
+def add_item(request, item_id):
+    size = request.POST['size']
+    quantity = int(request.POST['quantity'])
+    url = request.POST['url']
+    bag = request.session.get('bag', {})
+    if item_id in list(bag.keys()):
+        if size in bag[item_id]['size'].keys():
+            bag[item_id]['size'][size] += quantity
+        else:
+            bag[item_id]['size'][size] = quantity
+    else:
+        bag[item_id] = {'size': {size: quantity}}
+    request.session['bag'] = bag
+    return redirect(url)
