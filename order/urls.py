e51222e12cf289a152236ecc82285d0043ce3e48
bencheee <sandro.bencinic@gmail.com>
2022-01-05 12:57:13 +0000

    Update functions to plus, minus and delete items from bag

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import order, add_item
+from .views import order, add_item, plus_item, minus_item, delete_item
 
 urlpatterns = [
     path('', order, name='order'),
     path('add_item/<item_id>', add_item, name='add_item'),
+    path('plus_item/<item_id>/<size>', plus_item, name='plus_item'),
+    path('minus_item/<item_id>/<size>', minus_item, name='minus_item'),
+    path('delete_item/<item_id>/<size>', delete_item, name='delete_item'),
 ]
--- order/views.py
+++ order/views.py
@@ -19,3 +19,39 @@ def add_item(request, item_id):
         bag[item_id] = {'size': {size: quantity}}
     request.session['bag'] = bag
     return redirect(url)
+
+
+def plus_item(request, item_id, size):
+    bag = request.session.get('bag', {})
+    if item_id in list(bag.keys()):
+        if size in bag[item_id]['size'].keys():
+            bag[item_id]['size'][size] += 1
+
+    request.session['bag'] = bag
+    return redirect('order')
+
+
+def minus_item(request, item_id, size):
+    bag = request.session.get('bag', {})
+    if item_id in list(bag.keys()):
+        if size in bag[item_id]['size'].keys():
+            if bag[item_id]['size'][size] > 1:
+                bag[item_id]['size'][size] -= 1
+            else:
+                bag[item_id]['size'][size] = 1
+    request.session['bag'] = bag
+    return redirect('order')
+
+
+def delete_item(request, item_id, size):
+    bag = request.session.get('bag', {})
+    if item_id in list(bag.keys()):
+        if size in bag[item_id]['size'].keys():
+            del bag[item_id]['size'][size]
+
+    # Empty the cart object
+    if len(bag[item_id]['size'].keys()) == 0:
+        del bag[item_id]
+
+    request.session['bag'] = bag
+    return redirect('order')
