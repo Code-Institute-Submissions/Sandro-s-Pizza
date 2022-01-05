6df0411e0b3209f9fe6e16d09e80b8a34839f4d5
bencheee <sandro.bencinic@gmail.com>
2022-01-05 13:29:14 +0000

    Update checkout template to render context variables

--- order/contexts.py
+++ order/contexts.py
@@ -1,10 +1,14 @@
 from django.shortcuts import get_object_or_404
 from product.models import Item
+import datetime
 
 
 def contexts(request):
     bag_items = []
     grand_total = 0
+    delivery_fee = 2
+    grand_total += delivery_fee
+    order_time = None
     bag = request.session.get('bag', {})
     for item_id, item_data in bag.items():
         product = get_object_or_404(Item, pk=item_id)
@@ -16,15 +20,17 @@ def contexts(request):
             else:
                 total = quantity * product.price
             grand_total += total
+            order_time = datetime.datetime.now()
             bag_items.append({
                 'item_id': item_id,
                 'quantity': quantity,
                 'product': product,
                 'size': size,
-                'total': total
-            })
+                'total': total            })
     context = {
         'bag_items': bag_items,
         'grand_total': grand_total,
+        'order_time': order_time,
+        'delivery_fee': delivery_fee,
     }
     return context
