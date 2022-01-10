5eba721ef196cf71d5c72d70095ce64709f4fd6d
bencheee <sandro.bencinic@gmail.com>
2022-01-10 17:57:44 +0000

    Add form to checkout template and update form styles

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 
 def checkout(request):
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag is empty at the moment!")
+        return redirect(reverse('menu'))
+    form = OrderForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'checkout/checkout.html', context)
 
 
 def checkout_complete(request):
     return render(request, 'checkout/checkout_complete.html')
-
