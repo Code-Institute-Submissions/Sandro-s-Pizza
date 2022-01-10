0dbd8af4d1c0f59f042b9e62c82ef837b1f69338
bencheee <sandro.bencinic@gmail.com>
2022-01-10 17:01:34 +0000

    Add Order and OrderItem models to checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_total', 'date', 'order_number', 'original_bag', 'stripe_pid')
+
+class OrderItemAdmin(admin.ModelAdmin):
+    readonly_fields = ('orderitem_total',)
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,43 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from product.models import Item
 
-# Create your models here.
+
+class Order(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    street_address1 = models.CharField(max_length=100)
+    street_address2 = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    order_number = models.CharField(max_length=32, editable=False)
+    order_total = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, null=True)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def update_total(self):
+        self.order_total = self.orderitems.aggregate(
+            Sum('orderitem_total'))['orderitem_total__sum'] or 0
+        self.save()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='orderitems')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item_size = models.CharField(max_length=10)
+    quantity = models.IntegerField(default=0)
+    orderitem_total = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, default=0)
