f95eaeeda5e58fd9ce853e787cc0326b987086f5
bencheee <sandro.bencinic@gmail.com>
2022-01-11 09:56:46 +0000

    Add stripe payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
 from django.contrib import messages
 from .forms import OrderForm
+from order.contexts import contexts
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is empty at the moment!")
         return redirect(reverse('menu'))
     form = OrderForm()
 
+    current_bag = contexts(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
+
     context = {
         'form': form,
-        'stripe_public_key': "pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O",
-        'client_secret': "test client secret"
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, 'checkout/checkout.html', context)
 
--- static/js/checkout.js
+++ static/js/checkout.js
@@ -7,7 +7,7 @@
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
+var stripe = Stripe("stripePublicKey");
 var elements = stripe.elements();
 var style = {
     base: {
@@ -26,3 +26,19 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
