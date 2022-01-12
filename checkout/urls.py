79a8487b2b2f3813fe59dff34798b3ba66fb6aa6
bencheee <sandro.bencinic@gmail.com>
2022-01-12 10:51:20 +0000

    Add cache checkout data function to urls.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import checkout, checkout_complete
+from .views import checkout, checkout_complete, cache_checkout_data
 from .webhooks import webhook
 
 urlpatterns = [
     path('', checkout, name='checkout'),
     path('checkout_complete/<order_number>', checkout_complete, name='checkout_complete'),
+    path('cache_checkout_data/', cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
