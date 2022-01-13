fc634565f4091d33d8b60a90217ff3d42b46b561
bencheee <sandro.bencinic@gmail.com>
2022-01-13 17:30:39 +0000

    Style add/edit product forms

--- product/forms.py
+++ product/forms.py
@@ -1,7 +1,6 @@
-
-  
 from django import forms
 from .models import Item
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -9,3 +8,13 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Product name'}),
+            'ingredients': forms.TextInput(
+                attrs={'placeholder': '(Ingredients)'}),
+            'price': forms.TextInput(
+                attrs={'placeholder': 'price'}),
+        }
+    
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
