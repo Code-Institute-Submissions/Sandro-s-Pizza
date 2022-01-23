e320f16bd680a426ef4f269a94d7a2b1187e13f5
bencheee <sandro.bencinic@gmail.com>
2022-01-23 13:51:48 +0000

    Export carousel js to new file because it was causing issues with some event listeners

---
+++ static/js/carousel.js
@@ -0,0 +1,37 @@
+// Carousel on index page on (refreshes every 5 sec)
+// CODE CREDIT: https://github.com/karlhadwen/carousel/blob/master/app.js
+let carousel = document.getElementsByClassName("pizza-carousel__item")
+let carouselPosition = 0
+let carouselLength = carousel.length
+
+if (carousel.length > 0) {
+    setInterval(moveToNextSlide, 5000);
+}
+
+function updateCarouselPosition() {
+    for (let item of carousel) {
+        item.classList.remove('pizza-carousel__item--active');
+    }
+    carousel[carouselPosition].classList.add('pizza-carousel__item--active');
+}
+
+function moveToNextSlide() {
+    if (carouselPosition === carouselLength - 1) {
+        carouselPosition = 0;
+    } else {
+        carouselPosition++;
+    }
+    updateCarouselPosition();
+}
+
+function moveToPrevSlide() {
+    if (carouselPosition === 0) {
+        carouselPosition = carouselLength - 1;
+    } else {
+        carouselPosition--;
+    }
+    updateCarouselPosition();
+}
+
+// Adds active class to first item in carousel image on load
+$(".pizza-carousel__item")[0].classList.add('pizza-carousel__item--active');
--- static/js/script.js
+++ static/js/script.js
@@ -32,40 +32,7 @@ $(".js-back").click(function () {
     window.location.href = document.referrer;
 });
 
-// Carousel on index page on (refreshes every 5 sec)
-// CODE CREDIT: https://github.com/karlhadwen/carousel/blob/master/app.js
-let carousel = document.getElementsByClassName("pizza-carousel__item")
-let carouselPosition = 0
-let carouselLength = carousel.length
 
-if (carousel.length > 0) {
-    setInterval(moveToNextSlide, 5000);
-}
-
-function updateCarouselPosition() {
-    for (let item of carousel) {
-        item.classList.remove('pizza-carousel__item--active');
-    }
-    carousel[carouselPosition].classList.add('pizza-carousel__item--active');
-}
-
-function moveToNextSlide() {
-    if (carouselPosition === carouselLength - 1) {
-        carouselPosition = 0;
-    } else {
-        carouselPosition++;
-    }
-    updateCarouselPosition();
-}
-
-function moveToPrevSlide() {
-    if (carouselPosition === 0) {
-        carouselPosition = carouselLength - 1;
-    } else {
-        carouselPosition--;
-    }
-    updateCarouselPosition();
-}
 
 // Calculates total price on item page
 let defaultItemPrice = Number($("#item-price").text());
@@ -93,18 +60,12 @@ $(".checkout-popup__close").click(function(){
     $(".checkout-popup").hide()
 })
 
-// $(document).on("click", ".checkout-popup__close", $(".checkout-popup").hide());
-
 // Hides flash message
 $(".flash__close").click(function(){
     $(".flash").hide();
 })
 
-// Adds active class to first item in carousel image on load
-$(".pizza-carousel__item")[0].classList.add('pizza-carousel__item--active');
-
 // Adds event listener to nav toggle button on mobile
 $(".navbar__button").click(function () {
-    console.log("clicked");
     $(".navbar__mobile-menu").toggle("slide");
 })
